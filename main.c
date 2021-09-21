fedd27eac39ad9fff8248c5c3b24878d6cce27cb
Naween Pasindu <37021586+Naween-Pasindu@users.noreply.github.com>
2021-09-21 19:57:23 +0530

    Initialisation of window

--- main.c
+++ main.c
@@ -1,8 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include <GL/glut.h>
 
-int main()
+void display(void){
+}
+
+int main(int argc,char **argv)
 {
-    printf("Hello world!\n");
-    return 0;
+ glutInit(&argc,argv);
+ glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGB|GLUT_DEPTH);
+ glutInitWindowSize(500,500);
+ glutCreateWindow("My Game");
+ glutDisplayFunc(display);
+ glutMainLoop();
+ return(0);
 }
